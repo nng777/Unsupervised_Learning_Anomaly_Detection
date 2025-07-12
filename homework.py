3f1d7eac8caeb2994a22a889ae0e321740768fc2
nng777 <nikodemusng@gmail.com>
2025-07-12 10:25:47 +0700

    Commit

---
+++ homework.py
@@ -0,0 +1,8 @@
+The Data
+Modify customer_behavior_analyzer.py to analyze these influencer metrics:
+
+followers_count (1K-1M)
+posts_per_week (1-20)
+avg_likes_per_post (10-50K)
+avg_comments_per_post (5-2K)
+engagement_rate (1-15%)
