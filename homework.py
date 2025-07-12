b2daa0ce5c997cbf91a4d694c9ed23f7d94b4c07
nng777 <nikodemusng@gmail.com>
2025-07-12 11:18:10 +0700

    Commit

--- homework.py
+++ homework.py
@@ -1,13 +1,11 @@
-"""
-Task 1
+"""Task 1
 
 Step 1: Modify customer_behavior_analyzer.py to analyze these influencer metrics:
-
-followers_count (1K-1M)
-posts_per_week (1-20)
-avg_likes_per_post (10-50K)
-avg_comments_per_post (5-2K)
-engagement_rate (1-15%)
+1.followers_count (1K-1M)
+2.posts_per_week (1-20)
+3.avg_likes_per_post (10-50K)
+4.avg_comments_per_post (5-2K)
+5.engagement_rate (1-15%)
 
 Step 2: Generate 500 influencer profiles with 3 natural segments:
 1.Micro (1K-10K followers)
